e342593dc7bfab8b116829a459743bc6fc40fc31
Liza <bliza0812@GMAIL.COM>
2022-04-03 18:54:36 +0400

    1 лаба Защиты инфы

---
+++ Sait2022/ViewModels/Account/LockoutUsersViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sait2022.ViewModels.Account
+{
+    public class LockoutUsersViewModel
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public bool IsLocked { get; set; }
+
+        public DateTimeOffset? TimeLockout { get;set; }
+    }
+}
