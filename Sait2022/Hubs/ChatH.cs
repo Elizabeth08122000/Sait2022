b033e7056e26dcadc6153c19cd2637627fc894e2
Liza <bliza0812@GMAIL.COM>
2022-05-20 13:21:39 +0400

    удалила ненужный проект

--- Sait2022/Hubs/ChatH.cs
+++ Sait2022/Hubs/ChatH.cs
@@ -9,18 +9,6 @@ namespace Sait2022.Hubs
         public class ChatH : Hub
         {
         string groupname = "all";
-        //public async Task Enter(string username)
-        //{
-        //    if (String.IsNullOrEmpty(username))
-        //    {
-        //        await Clients.Caller.SendAsync("Notify", "Для входа в чат введите логин");
-        //    }
-        //    else
-        //    {
-        //        await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
-        //        await Clients.Group(groupname).SendAsync("Notify", $"{username} вошел в чат");
-        //    }
-        //}
         public async Task Send(string message, string username)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
