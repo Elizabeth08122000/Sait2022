bcc935a974669f0f8e43bf8b687dd6414ee9ef64
Елизавета Байгушкина <baigushkina.e@dobrozaim.ru>
2022-05-30 18:49:21 +0400

    юнит тест

--- AccountTests/UnitTest1.cs
+++ AccountTests/UnitTest1.cs
@@ -1,18 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sait2022;
 using Sait2022.Controllers;
-using Sait2022.Domain.Model;
 
 namespace AccountTests
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly ILogger<HomeController> _logger;
+
         [TestMethod]
-        public void TestDetailsView()
+        public void TestIndexView()
         {
-            
+            var controller = new HomeController(_logger);
+            Assert.IsNotNull(controller.Index());
+        }
+
+        [TestMethod]
+        public void TestPrivacyView()
+        {
+            var controller = new HomeController(_logger);
+            Assert.IsNotNull(controller.Privacy());
         }
     }
 }
